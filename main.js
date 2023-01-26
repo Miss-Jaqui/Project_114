8c93232479a0af559d9d76b1bd25db0bc9844341
Miss-Jaqui <107520551+Miss-Jaqui@users.noreply.github.com>
2023-01-26 16:39:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(350, 350);
+  canvas.center();
+}
+
+function draw() {
+
+}
+
+function Save_image(){    
+  save('myFilterImage.png');
+}
